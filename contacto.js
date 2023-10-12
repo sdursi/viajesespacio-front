100c61ad33a3e4ebe9c56e5b0fd9a342892df602
carolina keimichel <caropna@hotmail.com>
2023-10-12 20:30:04 -0300

    subo cambios

--- contacto.js
+++ contacto.js
@@ -1,20 +1,51 @@
-const from = document.getElementById(myForm);
-from.addEventListener(submit, function (event) {
-    event.preventDefault();
-    validateForm();
-});
+//Validación de un formulario con Javascript
+        //Fuente: https://desarrolloweb.com/articulos/1767.php
+        document.addEventListener("DOMContentLoaded", function () {
+            document.getElementById("formulario").addEventListener('submit', validarFormulario);
+        });
 
-function validateEmail(email) {
-    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,7}$/
-    return regex.test(email)
-}
-function validateForm() {
-    const emailinput = document.getElementById(email)
-    const email = emailinput.ariaValueMax;
+        function validarFormulario(evento) {
+            evento.preventDefault();
+            var nombre = document.getElementById('nombre').value;
+            if (nombre.length == 0) {
+                alert('No has escrito nada en el nombre');
+                return;
+            }
+            var apellido = document.getElementById('apellido').value;
+            if (apellido.length == 0) {
+                alert('No has escrito nada en apellido');
+                return;
+            }
 
-    if (!validateEmail(email)){
-        alert("Por favor ingrese un correo eletronico valido");
-    } else{
-        alert("correo electronico enviado correctamente");
-    }
-    }
+            function validateEmail(email) {
+                const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,7}$/
+                return regex.test(email)
+                }
+
+                function validateForm() {
+                    const emailInput = document.getElementById('email');
+                    const email = emailInput.value;
+                    if (!validateEmail(email)) {
+                    alert('Por favor ingrese un correo electrónico válido.');
+                    } else {
+                    alert('Correo electrónico enviado correctamente.');
+                    }
+                }
+            var numero = document.getElementById('numero').value;
+            if (numero.length ==0){
+                alert('No has indicado bien tu contacto').value;
+                return;
+            }
+            var fecha = document.getElementById('fecha').value;
+            if (fecha.length ==0){
+                alert('indica tu fecha').value;
+                return;
+            }
+			var submit = window.confirm('Enviar');
+            if (resultado === true){
+                windows.alert('estas seguro');{
+                }false
+                    windows.alert('pareces indeciso');    
+		
+			}
+            this.submit();}
